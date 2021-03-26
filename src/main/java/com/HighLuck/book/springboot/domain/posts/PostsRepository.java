bc4c25ed70fde1b916c1c051f873697a44bbfddd
Sang-un <dltkddns79@gmail.com>
2021-03-26 18:13:02 +0900

    PostsRepository query 를 통한 전체조회 화면 만들기

--- src/main/java/com/HighLuck/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/HighLuck/book/springboot/domain/posts/PostsRepository.java
@@ -1,10 +1,16 @@
 package com.HighLuck.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // post class 로 database 에 접근시켜주는 Interface
 
 // <entity type, pk class>
-public interface PostsRepository extends JpaRepository<Posts,Long> {
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    // 조회 할때 querydsl 을 추천함
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/HighLuck/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/HighLuck/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.HighLuck.book.springboot.service.posts;
 
 import com.HighLuck.book.springboot.domain.posts.Posts;
 import com.HighLuck.book.springboot.domain.posts.PostsRepository;
+import com.HighLuck.book.springboot.web.dto.PostsListResponseDto;
 import com.HighLuck.book.springboot.web.dto.PostsResponseDto;
 import com.HighLuck.book.springboot.web.dto.PostsSaveRequestDto;
 import com.HighLuck.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -27,9 +31,17 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
-    public PostsResponseDto findById(Long id){
-        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // transaction 범위는 유지하며 조회기능만 남겨두어 조회속도가 개선됭
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
