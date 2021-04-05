b9511c37b0d87d23410eac8379f7fdccb77e6227
Sang-un <dltkddns79@gmail.com>
2021-04-06 01:51:45 +0900

    oauth2 google && naver login test code oauth2 edit

--- src/test/java/com/HighLuck/book/springboot/HelloControllerTest.java
+++ src/test/java/com/HighLuck/book/springboot/HelloControllerTest.java
@@ -2,10 +2,15 @@ package com.HighLuck.book.springboot;
 
 // import com.HighLuck.book.springboot.web.HelloController;
 
+import com.HighLuck.book.springboot.config.auth.SecurityConfig;
+import com.HighLuck.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,14 +23,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class) // 스프링부트와 JUnit 사이에 연결자 역할
 //@WebMvcTest(controllers = HelloController.class) // controller 위주의 webMVC에 집중하게 하는 어노테이션
-@WebMvcTest
-public class HelloControllerTest {
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)public class HelloControllerTest {
 
     @Autowired // 스프링이 관리하는 빈을 주입받음
     private MockMvc mvc; // 웹 api를 테스트할때 사용
     // 테스트의 시작점
     // 이 클래스가 http get, post 등의 api 테스트를 할수있게 해줌
-
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -36,7 +44,7 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())         // 결과를 검증, 200인지 검증
                 .andExpect(content().string(hello)); // hello로 결과를 받는지 아닌지 검사
     }
-
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
