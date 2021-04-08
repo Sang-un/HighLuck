56c886f38ba8fbc8e2ee8ee80ba9400fdb423f2d
Sang-un <dltkddns79@gmail.com>
2021-04-09 00:34:18 +0900

    test postRepository

--- src/test/java/com/HighLuck/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/HighLuck/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,4 @@
 package com.HighLuck.book.springboot.domain.posts;
-
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,19 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest // 자동으로 h2 가 실행됨
+@SpringBootTest
 public class PostsRepositoryTest {
+
     @Autowired
     PostsRepository postsRepository;
 
-    @After // 단위 테스트가 끝날때 마다 실행해주는 코드 (테스트간 데이터 침법을 막기위해서)
+    @After
     public void cleanup() {
         postsRepository.deleteAll();
     }
@@ -30,9 +29,11 @@ public class PostsRepositoryTest {
         String title = "테스트 게시글";
         String content = "테스트 본문";
 
-        // post 의 데이터 insert, update 를 실행하는 코드
-        postsRepository.save(Posts.builder().title(title).content(content).author("dltkddns79@gmail.com").build());
-//        postsRepository.save(new Posts(title,content,"dltkddns79@gmail.com"));
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
 
         //when
         List<Posts> postsList = postsRepository.findAll();
@@ -48,18 +49,19 @@ public class PostsRepositoryTest {
         //given
         LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
         postsRepository.save(Posts.builder()
-                .title("title").content("content").author("author").build());
-
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
         //when
         List<Posts> postsList = postsRepository.findAll();
 
         //then
         Posts posts = postsList.get(0);
 
-        System.out.println(">>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+        System.out.println(">>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
 
         assertThat(posts.getCreatedDate()).isAfter(now);
         assertThat(posts.getModifiedDate()).isAfter(now);
-
     }
 }
